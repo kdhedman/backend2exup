a30cd1ee00af21eb85fbee039cd8c3901909af3d
kdhedman <kenth.david.hedman@gmail.com>
2021-09-16 13:13:24 +0200

    Added Swagger and Dockerfile

--- src/main/java/se/nackademin/backend2exup/Backend2exupApplication.java
+++ src/main/java/se/nackademin/backend2exup/Backend2exupApplication.java
@@ -2,7 +2,9 @@ package se.nackademin.backend2exup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class Backend2exupApplication {
 
